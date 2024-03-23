7c3901e503ba4ed439c2f77183eb911328ddc7cb
mohamed-shahien <shaheenagain@gmail.com>
2024-03-24 00:16:57 +0200

    setup themes and contanuo  landing page

---
+++ digitalstore/src/app/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        
+      </div>
+    </MaxWidthWrapper>
+  );
+}
