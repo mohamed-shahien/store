09a298ad1b86cff28a279e56e768e09222deb03c
mohamed-shahien <shaheenagain@gmail.com>
2024-07-09 23:42:18 +0300

    a

--- digitalstore/src/components/Cart.tsx
+++ digitalstore/src/components/Cart.tsx
@@ -1,22 +1,46 @@
 import { ShoppingCart } from "lucide-react"
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger , } from "./ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger, } from "./ui/sheet"
+import { Separator } from "./ui/separator";
 
 export const Cart = () => {
-        const itemCount = 1
-  return (
-    <Sheet>
-        <SheetTrigger className="group -m-2 flex items-center p-2">
-                <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
-                <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                        0
-                </span>
-        </SheetTrigger>
-        <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
-                <SheetHeader className="space-x-2.5 pr-6">
-                        <SheetTitle>Cart (0)</SheetTitle>
-                </SheetHeader>
-                {itemCount > 1 }
-        </SheetContent>
-    </Sheet>
-  )
+        const itemCount = 1;
+        return (
+                <Sheet>
+                        <SheetTrigger className="group -m-2 flex items-center p-2">
+                                <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+                                <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                        </SheetTrigger>
+                        <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+                                <SheetHeader className="space-x-2.5 pr-6">
+                                        <SheetTitle>Cart (0)</SheetTitle>
+                                </SheetHeader>
+                                {itemCount > 0 ? (
+                                        <>
+                                                <div className="flex w-full flex-col pr-6">
+                                                        cart item
+                                                </div>
+                                                <div className="space-y-4 pr-6">
+                                                        <Separator />
+                                                </div>
+                                                <div className="space-y-1.5 text-sm">
+                                                        <div className="flex w-full flex-col pr-6">
+                                                                <div className="flex">
+                                                                        <div className="flex-1">Shipping</div>
+                                                                        <span>Free</span>
+                                                                </div>
+                                                                <div className="flex">
+                                                                        <div className="flex-1">Transaction fee</div>
+                                                                        <span>1</span>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </>
+                                ) : (
+                                        <>
+
+                                        </>
+                                )}
+                        </SheetContent>
+                </Sheet>
+        )
 }
--- digitalstore/src/lib/utils.ts
+++ digitalstore/src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function formatePrice(
+  price: number | string ,
+  options: {
+    currency?: "AED" | "SAR" | "EGP" | "USD " ,
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+  // إذا لم يتم توفير currency أو notation في options، سيتم استخدام القيم الافتراضية "USD" و "compact" على التوالي.
+  const {currency = 'USD' , notation ='compact' } = options
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+  return new Intl.NumberFormat('en-us' , {
+    style : 'currency',
+    currency,
+    notation,
+    maximumSignificantDigits: 2,
+  }).format(numericPrice)
+}
+
+// https://chatgpt.com/share/dc5bf39b-9106-455d-9204-a9762b2d8b92
